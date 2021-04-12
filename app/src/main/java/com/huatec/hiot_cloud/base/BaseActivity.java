ef79785e9d596e93ae9bf24523b6aff8a99d6cec
li <214751202@qq.com>
2021-04-12 16:52:39 +0800

    更新BaseActivity,并测试注入

--- app/src/main/java/com/huatec/hiot_cloud/base/BaseActivity.java
+++ app/src/main/java/com/huatec/hiot_cloud/base/BaseActivity.java
@@ -1,9 +1,16 @@
 package com.huatec.hiot_cloud.base;
 
+import android.app.Application;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.huatec.hiot_cloud.App;
+import com.huatec.hiot_cloud.injection.component.ActivityComponent;
+import com.huatec.hiot_cloud.injection.component.ApplicationComponent;
+import com.huatec.hiot_cloud.injection.component.DaggerActivityComponent;
+import com.huatec.hiot_cloud.injection.module.ActivityModule;
 
 /**
  * MVP架构Activity基类
@@ -22,6 +29,11 @@ public abstract class BaseActivity<V extends BaseView, P extends BasePresenter> 
 
 
     public abstract P createPresenter();
+
+    /**
+     * 活动注入器
+     */
+    private ActivityComponent mActivityComponent;
 
     @Override
     protected void onDestroy() {
@@ -43,5 +55,31 @@ public abstract class BaseActivity<V extends BaseView, P extends BasePresenter> 
     @Override
     protected void onResume() {
         super.onResume();
+    }
+
+
+
+    public ActivityComponent getActivityComponent() {
+        if (null == mActivityComponent) {
+            mActivityComponent = DaggerActivityComponent.builder()
+                    .activityModule(getActivityModule())
+                    .applicationComponent(getApplicationComponent())
+                    .build();
+        }
+        return mActivityComponent;
+    }
+
+    public ApplicationComponent getApplicationComponent() {
+
+        Application application = getApplication();
+        App app = (App) application;
+        return app.component();
+    }
+
+    /**
+     * Get an Activity module for dependency injection.
+     */
+    protected ActivityModule getActivityModule() {
+        return new ActivityModule(this);
     }
 }
